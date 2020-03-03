63ab61023fe4cd815c40f5b207201c55ebcb2718
kevin Hoquet <hoquet_k@etna-alternance.net>
2020-03-03 22:36:55 +0100

    change API

--- api.js
+++ api.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:3000/api/'
-// export const API = 'https://ironikaspace.com/api/'
+// export const API = 'http://localhost:3000/api/'
+export const API = 'https://ironikaspace.com/api/'
