52c127de65cd14edac10d250ae165555f02d2f4b
Kévin Hoquet <kevin.hoquet@digitaslbi.fr>
2020-03-10 14:36:57 +0100

    chnage design card news

--- components/CardNews.js
+++ components/CardNews.js
@@ -6,19 +6,24 @@ import NOT_FOUND from '../public/img/notfound.png'
 
 const CardNews = (props) => {
     const pubDate = new Date(props.data.pubDate)
-    const html = (props.data.desc && props.data.desc.length > 150) ? props.data.desc.substring(0,150) + '...' : props.data.desc
+    const title = (props.data.title && props.data.title.length > 55) ? props.data.title.substring(0,55) + '...' : props.data.title
+    // const html = (props.data.desc && props.data.desc.length > 150) ? props.data.desc.substring(0,150) + '...' : props.data.desc
     return (
         <div className="card-news">
             <a href={props.data.link} target="_blank" rel="noopener noreferrer">
                 <LazyLoad placeholder={<Loader />}>
                     <img src={props.data.img ? props.data.img : NOT_FOUND} alt={props.data.title}/>
                 </LazyLoad>
-                <div className="card-news-content">
+                <svg class="card__svg" viewBox="0 0 800 500">
+                    <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent"/>
+                    <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
+                </svg>
+                {/*<div className="card-news-content">*/}
                     <button>{props.data.tag}</button>
                     <span className="date">{pubDate.toLocaleDateString('fr-FR')}</span>
-                    <h3>{props.data.title}</h3>
-                    <p dangerouslySetInnerHTML={{__html: html.replace(/(<? *script)/gi, 'illegalscript')}} ></p>
-                </div>
+                    <h3>{title}</h3>
+                    {/*<p dangerouslySetInnerHTML={{__html: html.replace(/(<? *script)/gi, 'illegalscript')}} ></p>*/}
+                {/*</div>*/}
             </a>
         </div>
     )
