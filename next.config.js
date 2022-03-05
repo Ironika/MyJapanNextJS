51b1321d2ab622b978c69ab90b5684d3c098228c
Hoquet Kevin <hoquet_k@etna-alternance.net>
2022-03-05 15:38:39 +0100

    fix domain since it expire

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 module.exports = withPlugins([withSass, withImages, withFonts], {
     env: {
-        api: process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/api/' : 'https://ironikaspace.com/api/',
+        api: process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/api/' : 'https://my-japan-next-js.vercel.app/api/',
         mongo_api_pwd: process.env.mongo_api_pwd,
         mailer_user: process.env.mailer_user,
         mailer_pwd: process.env.mailer_pwd
