0322864c644459c6f9e662d18b22b97269bb05af
Ironika <hoquet_k@etna-alternance.net>
2023-05-31 16:49:28 +0200

    fix error mongoDb try1

--- pages/api/scansva.js
+++ pages/api/scansva.js
@@ -15,22 +15,20 @@ ScansVaHandler.get(async (req, res) => {
   if (search) {
     const scansListSearched = await getSearch(search);
     res.json(scansListSearched);
-  }
-
-  if (link) {
+  } else if (link) {
     const reader = await getScansReader(link);
     if (reader) res.json(reader);
+  } else {
+    let scans = null;
+    if (uid) {
+      const bookmark = await req.db
+        .collection("bookmarks")
+        .findOne({ user_id: uid });
+      if (bookmark) scans = bookmark.scans;
+    }
+    const scansList = await getScansVa(page, prevpage, scans, onlybookmark);
+    res.json(scansList);
   }
-
-  let scans = null;
-  if (uid) {
-    const bookmark = await req.db
-      .collection("bookmarks")
-      .findOne({ user_id: uid });
-    if (bookmark) scans = bookmark.scans;
-  }
-  const scansList = await getScansVa(page, prevpage, scans, onlybookmark);
-  res.json(scansList);
 });
 
 export default ScansVaHandler;
